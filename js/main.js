f58b865a3b38332dbedf39492cc9cefad08c57d4
Pavel <barabanofwork@yandex.ru>
2019-05-22 18:08:00 +0300

    Сделал всплытие модального окна и центрировал его

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 var doc = document;
 
 var btn = doc.getElementsByClassName('online-singup-btn');
-var modalwin = doc.getElementsByClassName('modal-window-root');
+var closeBtn = doc.getElementsByClassName('close');
+
+// КНОПКА ЗАПИСАТЬСЯ
+var modalwin = doc.getElementsByClassName('fixed-overlay');
 
   btn[0].addEventListener('click', function () {
     console.log(modalwin.classList);  
     modalwin[0].classList.toggle("hidden-win");
   });
 
+
+  closeBtn[0].addEventListener('click', function () {
+    console.log(modalwin.classList);  
+    modalwin[0].classList.add("hidden-win");
+  });
